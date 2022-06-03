d289fb0b82ecba04c5026b276b1e78230a412c2d
ranjangits <rk9122439340@gmail.com>
2022-06-03 10:00:18 +0530

    initial commit

---
+++ url.js
@@ -0,0 +1,3 @@
+// module.exports = "mongodb://localhost:27017"
+// module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/nodedb?retryWrites=true&w=majority"
+module.exports="mongodb+srv://admin:admin@cluster0.a6htk.mongodb.net/nodedb?retryWrites=true&w=majority"
